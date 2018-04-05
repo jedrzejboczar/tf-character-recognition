05d33ab0b5319585632f02562034401b9638a58a
Jędrzej Boczar <yendreij@gmail.com>
2018-04-06 00:09:11 +0200

    Added some kind of epochs logging (could be better)

--- log.py
+++ log.py
@@ -2,11 +2,25 @@ import logging
 
 __loggers = []
 
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    # def cprint(self, startCodes, text, **kwargs):
+    #     startString = ''.join(startCodes)
+    #     endString = self.ENDC * len(startCodes)
+    #     print(startString + text + endString, **kwargs)
 
 def getLogger(name):
     logger = logging.getLogger(name)
     console = logging.StreamHandler()
-    fmt = logging.Formatter('[%(levelname)s] (%(asctime)s) %(message)s')
+    fmt = logging.Formatter('{}[%(levelname)s] %(name)s (%(asctime)s) : %(message)s{}'.format(
+        Colors.BOLD, Colors.ENDC))
     console.setFormatter(fmt)
     logger.addHandler(console)
     __loggers.append(logger)
--- run.py
+++ run.py
@@ -16,16 +16,20 @@ import log
 
 # (note) for gpu usage monitoring: optirun nvidia-smi -l 2
 
+logger = log.getLogger('run_script')
+
 DEFAULTS = {
     'epochs': 1,
-    'batch_size': 100,
+    'batch-size': 100,
 }
 
 parser = argparse.ArgumentParser(description='TODO')
-parser.add_argument('-b', '--batch_size', type=int, default=DEFAULTS['batch_size'],
+parser.add_argument('-b', '--batch-size', type=int, default=DEFAULTS['batch-size'],
     help='Number of images in a batch')
 parser.add_argument('-e', '--epochs', type=int,  default=DEFAULTS['epochs'],
     help='Number of epochs of training (one epoch means traversing the whole training database)')
+parser.add_argument('-n', '--eval-each-n', type=int,  default=None,
+    help='Run evaluation each n epochs (disabled by default)')
 parser.add_argument('-T', '--train', action='store_true',
     help='Perform training using the training database')
 parser.add_argument('-E', '--eval', action='store_true',
@@ -41,7 +45,7 @@ parser.add_argument('-v', '--verbose', action='count', default=0,
 def main():
     args = parser.parse_args()
     if not (args.train or args.eval or args.predict or args.gui):
-        print('No action specified (see --help).')
+        logger.warn('No action specified (see --help).')
         return
 
     # configure verbosity
@@ -54,39 +58,54 @@ def main():
     estimator = cnn_model.get_estimator()
 
     if args.gui:
-        print('Using CPU only for better performance in GUI mode')
+        logger.info('Using CPU only for better performance in GUI mode')
         os.environ['CUDA_VISIBLE_DEVICES'] = ''
         gui.runApp(estimator)
 
-    def train_input_fn():
-        return database.get_train_dataset().shuffle(10000).batch(
-            args.batch_size).repeat(args.epochs).prefetch(1)
+    def train_input_fn(repeats):
+        return lambda : database.get_train_dataset().shuffle(10000).batch(
+            args.batch_size).repeat(repeats).prefetch(1)
 
     def eval_input_fn():
-        return database.get_test_dataset().batch(args.batch_size).prefetch(1)
+        return lambda : database.get_test_dataset().batch(args.batch_size).prefetch(1)
 
     def predict_input_fn():
-        return database.prepare_dataset(args.predict).batch(args.batch_size)
+        return lambda : database.prepare_dataset(args.predict).batch(args.batch_size)
 
     if args.train:
-        estimator.train(train_input_fn)
+        info_epochs = lambda _from, _to: logger.info('EPOCHS %d to %d:' % (_from, _to))
+        if args.eval_each_n:
+            n_full = args.epochs // args.eval_each_n if args.eval_each_n is not None else 0
+            for i in range(n_full):
+                info_epochs(i * args.eval_each_n + 1, (i+1) * args.eval_each_n)
+                estimator.train(train_input_fn(args.eval_each_n))
+                results = estimator.evaluate(eval_input_fn())
+                logger.info('Test data accuracy: %.3f' % results['accuracy'])
+            remaining_epochs = args.epochs - n_full * args.eval_each_n
+            if remaining_epochs > 0:
+                info_epochs(n_full * args.eval_each_n + 1, args.epochs)
+                estimator.train(train_input_fn(remaining_epochs))
+        else:
+            info_epochs(1, args.epochs)
+            estimator.train(train_input_fn(args.epochs))
+
 
     if args.eval:
-        results = estimator.evaluate(eval_input_fn)
-        print('Test data accuracy: %.3f' % results['accuracy'])
+        results = estimator.evaluate(eval_input_fn())
+        logger.info('Test data accuracy: %.3f' % results['accuracy'])
 
     if args.predict:
-        predictions = estimator.predict(predict_input_fn)
+        predictions = estimator.predict(predict_input_fn())
         common_path = os.path.split(os.path.commonprefix(args.predict))[0]
         filenames = [os.path.relpath(path, start=common_path) for path in args.predict]
         max_filename_len = max(len(name) for name in filenames)
 
-        print('Predictions:')
+        logger.info('Predictions:')
         for filename, prediction_dict in zip(filenames, predictions):
             pi = prediction_dict['predictions']
             label = database.CLASSES[pi]
             probability = prediction_dict['probabilities'][pi]
-            print('{name:>{nlen}}: {lab} ({prob:6.2f} %)'.format(name=filename,
+            logger.info('{name:>{nlen}}: {lab} ({prob:6.2f} %)'.format(name=filename,
                 nlen=max_filename_len, lab=label, prob=probability * 100))
 
 
