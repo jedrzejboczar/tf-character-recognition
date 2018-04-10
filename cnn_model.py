e7505902ffc3998b4965df7ee264759bd079c01a
Jędrzej Boczar <yendreij@gmail.com>
2018-04-10 21:46:37 +0200

    Added visualization of convolutional layers activations

--- cnn_model.py
+++ cnn_model.py
@@ -1,72 +1,170 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import cv2
+import numpy as np
 import tensorflow as tf
 
+import log
 import data
 
 
-def get_estimator():
-    return tf.estimator.Estimator(
-        model,
-        model_dir='models/cnn_v9_94x94',
-        params={},
-    )
+ModeKeys = tf.estimator.ModeKeys  # shorter
 
-def model(features, labels, mode, config, params):
+
+class Model:
     """
     Convolutional neural network model.
-        features - batch of 3-channel images, float32 (0-255)
-        labels - batch of labels (single int)
+    Class defines computation graph and provides model_fn for tf.estimator.Estimator.
     """
-    ModeKeys = tf.estimator.ModeKeys
-    images = features
+    def __init__(self):
+        self.logger = log.getLogger('model')
+        self.model_dir = 'models/cnn_v9_94x94'
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=0.0002)
+        self.layers = [
+            # Convolutional part
+            tf.layers.Conv2D(filters=8, kernel_size=3, activation=tf.nn.relu),   # [92, 92, 8]
+            tf.layers.MaxPooling2D(pool_size=2, strides=2),                      # [46, 46, 8]
+            tf.layers.Conv2D(filters=32, kernel_size=3, activation=tf.nn.relu),  # [44, 44, 8]
+            tf.layers.MaxPooling2D(pool_size=2, strides=2),                      # [22, 22, 8]
+            tf.layers.Conv2D(filters=72, kernel_size=3, activation=tf.nn.relu),  # [20, 20, 32]
+            tf.layers.MaxPooling2D(pool_size=2, strides=2),                      # [10, 10, 32]
+            tf.layers.Conv2D(filters=160, kernel_size=3, activation=tf.nn.relu), # [8, 8, 128]
+            tf.layers.MaxPooling2D(pool_size=2, strides=2),                      # [4, 4, 128]
+            tf.layers.Conv2D(filters=512, kernel_size=3, activation=tf.nn.relu), # [2, 2, 512]
+            tf.layers.MaxPooling2D(pool_size=2, strides=2),                      # [1, 1, 512]
+            # Dense part
+            tf.layers.Flatten(),                                                 # [512]
+            tf.layers.Dense(units=1024, activation=tf.nn.relu),                  # [1024]
+            tf.layers.Dropout(rate=.8),
+            tf.layers.Dense(units=data.Database.N_CLASSES),
+        ]
+        self.intermediate_outputs = []
 
-    tf.assert_equal(tf.shape(images)[1:3], data.Database.IMAGE_SIZE,
-        data=[tf.shape(images)], summarize=5, message='Wrong input images shape')
+    def get_estimator(self, **kwargs):
+        """Creates an instance of tf.estimator.Estimator for the model.
 
-    # convolutional part (dimensions without first (batch_size))
-    net = images  # [94, 94, 1]
-    net = tf.layers.conv2d(net, filters=8, kernel_size=3, activation=tf.nn.relu)  # [92, 92, 8]
-    net = tf.layers.max_pooling2d(net, 2, 2)  # [46, 46, 8]
-    net = tf.layers.conv2d(net, filters=32, kernel_size=3, activation=tf.nn.relu)  # [44, 44, 8]
-    net = tf.layers.max_pooling2d(net, 2, 2)  # [22, 22, 8]
-    net = tf.layers.conv2d(net, filters=72, kernel_size=3, activation=tf.nn.relu)  # [20, 20, 32]
-    net = tf.layers.max_pooling2d(net, 2, 2)  # [10, 10, 32]
-    net = tf.layers.conv2d(net, filters=160, kernel_size=3, activation=tf.nn.relu)  # [8, 8, 128]
-    net = tf.layers.max_pooling2d(net, 2, 2)  # [4, 4, 128]
-    net = tf.layers.conv2d(net, filters=512, kernel_size=3, activation=tf.nn.relu)  # [2, 2, 512]
-    net = tf.layers.max_pooling2d(net, 2, 2)  # [1, 1, 512]
+        If given, passes all keyword arguments to its constructor, else
+        it uses model's default values."""
+        return tf.estimator.Estimator(
+            self.model_fn,
+            model_dir=kwargs.get('model_dir', self.model_dir),
+            **kwargs,
+        )
 
-    assert net.shape[1:] == [1, 1, 512], 'Something is not yes! net.shape = %s' % net.shape
+    def model_fn(self, features, labels, mode, config=None, params={}):
+        # features - batch of 1-channel images, float32, 0-255
+        images = features
+        assert images.shape[1:3] == data.Database.IMAGE_SIZE, \
+            'Something is not yes! Wrong images.shape = %s' % images.shape
 
-    # dense part
-    net = tf.layers.flatten(net)  # [512]
-    net = tf.layers.dense(net, 1024, activation=tf.nn.relu)  # [1024]
-    net = tf.layers.dropout(net, rate=.8, training=mode == ModeKeys.TRAIN)
-    net = tf.layers.dense(net, data.Database.N_CLASSES)
-    logits = net
+        # assemble model output from all layers
+        self.logger.info('Building model...')
+        net = images
+        self.intermediate_outputs = []
+        self.logger.info('   %s' % net.shape)
+        for layer in self.layers:
+            # for dropout we have to specify if it is training mode
+            if isinstance(layer, tf.layers.Dropout):
+                net = layer(net, training=mode == ModeKeys.TRAIN)
+            else:
+                net = layer(net)
+            # save outputs of each layer
+            self.intermediate_outputs.append(net)
+            self.logger.info('   %s' % net.shape)
 
-    probabilities = tf.nn.softmax(logits)
+        # outputs (loss is computed if not in predict mode)
+        logits = net
+        probabilities = tf.nn.softmax(logits)
+        loss_fn = lambda : tf.losses.sparse_softmax_cross_entropy(labels, logits)
 
-    if mode == ModeKeys.PREDICT:
-        predictions = {
-            'predictions': tf.argmax(logits, axis=1),  # index of best prediction for each image
-            'logits': logits,
-            'probabilities': probabilities,
-            'top_indices': tf.nn.top_k(logits, k=data.Database.N_CLASSES).indices,
-        }
-        return tf.estimator.EstimatorSpec(mode, predictions)
+        # create EstimatorSpecs depending on mode
+        if mode == ModeKeys.PREDICT:
+            predictions = {
+                'predictions': tf.argmax(logits, axis=1),  # index of best prediction for each image
+                'logits': logits,
+                'probabilities': probabilities,
+                'top_indices': tf.nn.top_k(logits, k=data.Database.N_CLASSES).indices,
+            }
+            if params.get('store_images', False):
+                predictions['images'] = images
+            if params.get('store_intermediate', False):
+                intermediate_dict = {i: out for i, out in enumerate(self.intermediate_outputs)}
+                predictions.update(intermediate_dict)
+            return tf.estimator.EstimatorSpec(mode, predictions)
 
-    loss = tf.losses.sparse_softmax_cross_entropy(labels, logits)
+        loss = loss_fn()
 
-    if mode == ModeKeys.TRAIN:
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.0005)
-        optimization = optimizer.minimize(loss, global_step=tf.train.get_or_create_global_step())
-        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=optimization)
+        if mode == ModeKeys.TRAIN:
+            optimization = self.optimizer.minimize(loss, global_step=tf.train.get_or_create_global_step())
+            return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=optimization)
 
-    assert mode == ModeKeys.EVAL, 'Received unexpected mode: %s' % mode
-    metrics = {
-        'accuracy': tf.metrics.accuracy(labels, predictions=tf.argmax(logits, axis=1)),
-    }
-    return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
+        assert mode == ModeKeys.EVAL, 'Received unexpected mode: %s' % mode
+        metrics = {
+            'accuracy': tf.metrics.accuracy(labels, predictions=tf.argmax(logits, axis=1)), }
+        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
+
+    def show_layers_outputs(self, predict_input_fn):
+        """Shows outputs of intermediate layers according to specified filters
+
+        Format of 'filter_string':
+            <class_name> <nth_layer_of_that_type> <ith_filter> ...
+        examples:
+            1st conv2d, 3rd filter; 2nd conv2d, 3rd filter; 2nd max_pool2d, 3rd filter
+            'Conv2D 1 3 Conv2D 2 3 MaxPooling2D 2 3'
+            conv2ds from 2 to 4, filters from 1 to 9
+            'Conv2D 2:4 1:9'
+        """
+        # convenience function
+        def show_image(name, img):
+            cv2.imshow(name, img)
+            return not cv2.waitKey(0) in [27, ord('q')] # 'ESCAPE' or 'q'
+
+        def best_grid(N):
+            rows = np.floor(np.sqrt(N)) # floor => take less rows than columns
+            cols = np.ceil(N / rows)    # take so many cols to fit all elements
+            return int(rows), int(cols)
+
+        # as for now only for 1 image
+        params = {'store_intermediate': True, 'store_images': True}
+        predictions = self.get_estimator(params=params).predict(predict_input_fn)
+
+        # CNN-like layers
+        for prediction in predictions:
+            self.logger.info('Showing input image')
+            show_image('image', prediction['images'])
+            for i, layer in enumerate(self.layers):
+                if isinstance(layer, tf.layers.Conv2D):
+                    name = layer.__class__.__name__
+                    filters = prediction[i]
+                    n_filters = filters.shape[-1]
+                    self.logger.info('Showing layer %s nr %d - %d filters' % (name, i+1, n_filters))
+                    # create one big image from filter outputs
+                    padding = 2
+                    n_rows, n_cols = best_grid(n_filters)
+                    im_height, im_width = filters.shape[:2]
+                    image = 0.3 * np.ones([n_rows * im_height + (n_rows+1) * padding,
+                        n_cols * im_width + (n_cols+1) * padding])
+                    for row in range(n_rows):
+                        for col in range(n_cols):
+                            n = row * n_cols + col
+                            if n >= n_filters:
+                                continue
+                            # find the right index ranges in the 'image' matrix
+                            base_height = (row+1) * padding + row * im_height
+                            base_width = (col+1) * padding + col * im_width
+                            slice_h = slice(base_height, base_height + im_height)
+                            slice_w = slice(base_width, base_width + im_width)
+                            # normalize filter values to see activations
+                            filter = filters[:, :, n]
+                            eps = 1e-6
+                            filter = filter / (np.max(filter) + eps) * 255
+                            # insert filter image at its position
+                            image[slice_h, slice_w] = filter
+                    # increase image size to be visible
+                    desired_rows = 500
+                    scaling = desired_rows / image.shape[0]
+                    image = cv2.resize(image, None, fx=scaling, fy=scaling, interpolation=cv2.INTER_NEAREST)
+                    to_continue = show_image('image', image)
+                    if not to_continue:
+                        break
--- run.py
+++ run.py
@@ -25,6 +25,8 @@ DEFAULTS = {
 }
 
 parser = argparse.ArgumentParser(description='TODO')
+parser.add_argument('-v', '--verbose', action='count', default=0,
+    help='Increase verbosity ERROR -> WARN -> INFO -> DEBUG (for each usage of this argument)')
 parser.add_argument('-b', '--batch-size', type=int, default=DEFAULTS['batch-size'],
     help='Number of images in a batch')
 parser.add_argument('-e', '--epochs', type=int,  default=DEFAULTS['epochs'],
@@ -37,17 +39,17 @@ parser.add_argument('-E', '--eval', action='store_true',
     help='Evaluate accurancy on the test database')
 parser.add_argument('-P', '--predict', nargs='+', metavar='IMG_FILE',
     help='Perform prediction on given image files')
+parser.add_argument('-S', '--show', nargs='+', metavar='IMG_FILE',
+    help='Show intermediate outputs for given image')
 parser.add_argument('-G', '--gui', action='store_true',
     help='Run interactive prediction GUI (discards other options)')
-parser.add_argument('-v', '--verbose', action='count', default=0,
-    help='Increase verbosity ERROR -> WARN -> INFO -> DEBUG (for each usage of this argument)')
 
 
 def main():
     args = parser.parse_args()
     if not (args.train or args.eval or args.predict or args.gui):
         logger.warn('No action specified (see --help).')
-        return
+        # return
 
     # configure verbosity
     default = tf.logging.WARN
@@ -56,7 +58,8 @@ def main():
     log.setLevel(verbosity)
 
     database = data.Database()
-    estimator = cnn_model.get_estimator()
+    model = cnn_model.Model()
+    estimator = model.get_estimator()
 
     if args.gui:
         logger.info('Using CPU only for better performance in GUI mode')
@@ -70,8 +73,8 @@ def main():
     def eval_input_fn():
         return lambda : database.get_test_dataset().batch(args.batch_size).prefetch(1)
 
-    def predict_input_fn():
-        return lambda : database.prepare_dataset(args.predict).batch(args.batch_size)
+    def predict_input_fn(files):
+        return lambda : database.from_files(files).batch(args.batch_size)
 
     if args.train:
         info_epochs = lambda _from, _to: logger.info('EPOCHS from {} to {}:'.format(_from, _to))
@@ -99,11 +102,13 @@ def main():
             info_time(time.time() - start, args.epochs)
 
     if args.eval:
+        start = time.time()
         results = estimator.evaluate(eval_input_fn())
-        logger.info('Test data accuracy: %.3f' % results['accuracy'])
+        logger.info('Test data accuracy: %.3f (eval in %.2f sec)'
+            % (results['accuracy'], time.time() - start))
 
     if args.predict:
-        predictions = estimator.predict(predict_input_fn())
+        predictions = estimator.predict(predict_input_fn(args.predict))
         common_path = os.path.split(os.path.commonprefix(args.predict))[0]
         filenames = [os.path.relpath(path, start=common_path) for path in args.predict]
         max_filename_len = max(len(name) for name in filenames)
@@ -115,6 +120,10 @@ def main():
             probability = prediction_dict['probabilities'][pi]
             logger.info('{name:>{nlen}}: {lab} ({prob:6.2f} %)'.format(name=filename,
                 nlen=max_filename_len, lab=label, prob=probability * 100))
+
+    if args.show:
+        model.show_layers_outputs(predict_input_fn(args.show))
+
 
 
 if __name__ == '__main__':
